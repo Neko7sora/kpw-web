fe8fce7e2fc30e5e3d21ab9eadd52dcdc36a688e
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2022-05-25 22:17:04 +0900

    Update Website
    update dependencies
    update lighthouse
    Update manifest.json
    Update pwa

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@
 // to reduce http requests of small files
 'use strict';
 
+import './assets/js/bootstrap.js';
+
 // Prefetch in-viewport links during idle time
 import { listen } from 'quicklink/dist/quicklink.mjs';
 listen();
@@ -11,3 +13,8 @@ import 'lazysizes';
 
 // global alert
 import './assets/js/alert';
+
+import '@pwabuilder/pwaupdate/dist/pwa-update.js';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
