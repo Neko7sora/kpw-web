e4c424747760991d20465be024619d4e985a9f89
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2022-11-25 21:18:48 +0900

    Create package html-minifier

---
+++ packages/html-minifier/index.js
@@ -0,0 +1,25 @@
+const { src, dest } = require("gulp");
+const { minify } = require("html-minifier-terser");
+
+const options = {
+  collapseWhitespace: true,
+  html5: true,
+  minifyCSS: true,
+  minifyJS: true,
+  removeComments: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+};
+
+function html() {
+  return src("./../../public/**/**.html")
+    .on("data", async function (file) {
+      console.log(file.path);
+      const buferFile = Buffer.from(
+        await minify(await file.contents.toString(), options)
+      );
+      return (file.contents = buferFile);
+    })
+    .pipe(dest("./../../public"));
+}
+html();
