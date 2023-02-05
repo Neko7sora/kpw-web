77eb02515d5c681a8e9629218f1a43bfe0b09b72
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2023-02-06 00:42:53 +0900

    lint

--- apps/hugo-tailwild/assets/js/app.js
+++ apps/hugo-tailwild/assets/js/app.js
@@ -1,7 +1,7 @@
 // app.js for concatenation of smaller libraryies
 // to reduce http requests of small files
-'use strict';
+"use strict";
 
 // Prefetch in-viewport links during idle time
-import { listen } from 'quicklink/dist/quicklink.mjs';
+import { listen } from "quicklink/dist/quicklink.mjs";
 listen();
