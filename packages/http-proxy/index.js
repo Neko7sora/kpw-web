9521788658836c4a6f3d1983bfc295a3304f6486
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2023-02-08 23:35:05 +0900

    add http-proxy

---
+++ packages/http-proxy/index.js
@@ -0,0 +1,28 @@
+const Fastify = require('fastify')
+const server = Fastify()
+const proxy = require('@fastify/http-proxy')
+
+server.register(proxy, {
+  upstream: 'http://127.0.0.1:1313',
+  prefix: '/',
+  rewritePrefix: '/',
+  http2: false
+})
+
+server.register(proxy, {
+  upstream: 'http://127.0.0.1:8000',
+  prefix: '/docs',
+  rewritePrefix: '/docs',
+  http2: false
+})
+
+server.register(proxy, {
+  upstream: 'http://127.0.0.1:8000',
+  prefix: '/livereload',
+  rewritePrefix: '/livereload',
+  http2: false
+})
+
+server.listen({ port: 3000 })
+
+console.log("http-proxy: http://127.0.0.1:3000")
