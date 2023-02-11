7fa7dce7e2ba6560f0e51774bf467cd11b093531
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2023-02-11 17:20:31 +0900

    update pwa

--- apps/hugo-tailwild/assets/js/app.js
+++ apps/hugo-tailwild/assets/js/app.js
@@ -5,3 +5,8 @@
 // Prefetch in-viewport links during idle time
 import { listen } from "quicklink/dist/quicklink.mjs";
 listen();
+
+import "@pwabuilder/pwaupdate/dist/pwa-update.js";
+
+const el = document.createElement("pwa-update");
+document.body.appendChild(el);
